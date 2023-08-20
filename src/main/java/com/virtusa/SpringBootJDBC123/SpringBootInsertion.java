20c33f8fac195743924bc922a7c9a12457e7ebd0
vineeth924 <vineethkumar924@gmail.com>
2023-08-20 22:43:36 +0530

    springbootinsertion using JDBCTEMPLATE

---
+++ src/main/java/com/virtusa/SpringBootJDBC123/SpringBootInsertion.java
@@ -0,0 +1,19 @@
+package com.virtusa.SpringBootJDBC123;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootInsertion {
+	@Autowired
+	JdbcTemplate jdbc;
+	@RequestMapping("/insert")
+	public String insertrecord() {
+		String query="insert into product1(id,name) values('p102','vineeth')";
+		jdbc.execute(query);
+		return "data inserted"; 
+		
+	}
+}
